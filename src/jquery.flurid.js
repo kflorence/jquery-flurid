f2168922ac975141b16522e2542f263f665d3c05
Kyle Florence <kyle.florence@gmail.com>
2011-03-30 17:46:31 -0700

    Update to version 2.0

--- src/jquery.flurid.js
+++ src/jquery.flurid.js
@@ -1,10 +1,10 @@
 /**
- * @fileOverview jQuery.flurid
+ * jQuery.flurid
  *
  * Helper functions for the Flurid CSS Grid Framework
  *
  * @author Kyle Florence <kyle[dot]florence[at]gmail[dot]com>
- * @version 1.0.20110119
+ * @version 2.0.20110330
  */
 ;(function($, window) {
   /**
@@ -23,55 +23,21 @@
   $.fn.flurid = function(options) {
     options = $.extend({}, $.fn.flurid.options, options);
 
-    // Mimic Gecko sub-pixel rounding behavior
-    // http://ejohn.org/blog/sub-pixel-problems-in-css/
-    /* Work in Progress
-    if (options.fix_subpixel_rounding && $.resize) {
-      // Prevent multiple bindings
-      $(window).unbind("resize").bind("resize", function() {
-        // Now re-calculate and apply new widths
-        $(".grid").each(function() {
-          var $grid = $(this),
-            $rows = $(".row", $grid),
-            grid_width = $grid.width();
-
-          $rows.each(function(r, row) {
-            var $columns = $(".column", $(row)),
-              column_count = $columns.length;
-
-            $columns.each(function(c, col) {
-              if (grid_width % 2 !== 0 || column_count % 2 !== 0) {
-                var width = Math.round(grid_width / column_count)
-                  + (c % 2 === 0 ? 0 : 1);
-
-                $(col).width(width);
-              }
-            });
-          });
-        });
-      });
-    }
-    */
-
     return this.each(function() {
-      var $grid = $(this),
-        $rows = $(".row", $grid),
-        row_count = $rows.length;
+      var $rows = $(this).find(".row"),
+        rows = ($rows.length - 1);
 
-      // loops through rows
       $rows.each(function(r, row) {
-        var r = (r + 1),
-          $row = $(row),
-          $columns = $(".column", $row),
-          column_count = $columns.length,
+        var $row = $(row),
+          $columns = $row.find(".column"),
+          columns = ($columns.length - 1),
           tallest_column = 0;
 
-        // loops through columns
         $columns.each(function(c, col) {
-          var c = (c + 1),
-            $col = $(col),
-            height = $col.height(),
-            insidePushBlock = $col.parents("[class*=push]").length > 0;
+          var $col = $(col),
+            column_height = $col.height(),
+            shifted_column = $col.hasClass("shift")
+              || $col.parents(".shift").length > 0;
 
           // alternating columns
           if (options.alternate == "columns") {
@@ -79,27 +45,27 @@
           }
 
           // first / last column
-          if (c === 1) {
+          if (c === 0) {
             $col.addClass("first");
-          } else if (c === column_count && !insidePushBlock) {
+          } else if (c === columns && !shifted_column) {
             $col.addClass("last");
           }
 
           // store tallest column height
-          if (height > tallest_column) {
-            tallest_column = height;
+          if (column_height > tallest_column) {
+            tallest_column = column_height;
           }
         });
 
         // alternating rows
         if (options.alternate == "rows") {
-          $row.addClass((r % 2 == 0 ? "even" : "odd"));
+          $row.addClass(r % 2 == 0 ? "even" : "odd");
         }
 
         // first / last row
-        if (r === 1) {
+        if (r === 0) {
           $row.addClass("first");
-        } else if (r === row_count) {
+        } else if (r === rows) {
           $row.addClass("last");
         }
 
@@ -132,9 +98,7 @@
      * @default false
      * @type Boolean
      */
-    equal_height_columns: false,
-
-    fix_subpixel_rounding: true
+    equal_height_columns: false
   };
 
 })(jQuery, this);
